3d15668a1e41635824884ef0235ffecc63fceecd
AndreiFlitea <fliteaandrei@gmail.com>
2023-04-13 08:02:03 +0300

    .

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,12 @@
 <?php
 require "config/config.php";
 
-if(isset($_SESSION['username'])) {
-    $userLoggedIn = $_SESSION['username'];
+if(isset($_SESSION['username'])) { // check if the session variable "username" has been set 
+    $userLoggedIn = $_SESSION['username']; // if set then assign the value to a local variable called '$userLoggedIn'
+    $user_detail_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$userLoggedIn'"); // execute a SQL query to fetch details about the user from the database 
+    $user = mysqli_fetch_array($user_detail_query); // transform the result into an array.
 } else {
-    header("Location: register.php");
+    header("Location: register.php"); // else function that redirects
 }
 ?>
 
@@ -17,24 +19,38 @@ if(isset($_SESSION['username'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Faces!!!</title>
+    <!-- Javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script src="assets/js/bootstrap.js"></script>
+    <!-- CSS -->
+    <script src="https://kit.fontawesome.com/67fce18640.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">    
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">    
+
 
 </head>
 
 <body>    
 
-<!-- Example single danger button -->
-<div class="btn-group">
-  <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-    Action
-  </button>
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-    <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#">Separated link</a></li>
-  </ul>
+<div class="top_bar">
+  <div class="logo">
+    <a href="index.php">Faces!</a>
+  </div>
+
+  <nav>
+    <a href="#"><?php echo $user['first_name']; ?></i></i></a>
+    <a href="index.php"><i class="fa-solid fa-house"></i></i></a>
+    <a href="#"><i class="fa-solid fa-envelope"></i></a>
+    <a href="#"><i class="fa-solid fa-bell"></i></a>
+    <a href="#"><i class="fa-solid fa-users"></i></i></a>
+    <a href="#"><i class="fa-sharp fa-solid fa-gear"></i></a>
+    <a href="#"><i class="fa-solid fa-right-from-bracket"></i></a>
+
+  </nav>
 </div>
+
+
+<div class="wrapper">
+  
+
+
--- index.php
+++ index.php
@@ -3,8 +3,21 @@ include('includes/header.php');
 // session_destroy();
 ?>
 
+   
+<div class="user_details column">
+        <a href="#"><img src="<?php echo $user['profile_pic']; ?>" alt=""></a>
+        
+        <div class="user_details_left_right">
+            <a href="#"><?php echo $user['first_name'] . ' ' . $user['last_name']  ?></a>
+            <br>
+            <?php echo "Posts: " . $user['num_posts'] . "<br>";
+                  echo "Likes: " . $user['num_likes'];
+            ?>
+        </div>   
+    </div>
+
+</div>
 
 
-<h1>Welcome to Faces!!!</h1>
 </body>
 </html>
