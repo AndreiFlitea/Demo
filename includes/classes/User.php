1f1674dfda244a5d298234495770f8585651709a
AndreiFlitea <fliteaandrei@gmaile.com>
2023-04-17 22:21:17 +0300

    new

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -9,11 +9,38 @@ class User {
         $this->user = mysqli_fetch_array($user_details_query);
     }
 
+    public function getUsername(){
+        return $this->user['username'];
+    }
+
+    public function getNumPosts() {
+        $username = $this->user['username'];
+        $query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
+        $row = mysqli_fetch_array($query);
+        return $row['num_posts'];
+    }
+
     public function getFirstAndLastName() {
         $username = $this->user['username'];
         $query = mysqli_query($this->con, "SELECT first_name, last_name FROM users WHERE username = '$username'");
         $row = mysqli_fetch_array($query);
         return $row['first_name'] . ' ' . $row['last_name'];
+    }
+
+    public function isClosed() {
+        if (!isset($this->user['username'])) {
+            // Handle the case when 'username' is not set in $this->user
+            return false;
+        }
+        $username = $this->user['username'];
+        
+        $query = mysqli_query($this->con, "SELECT user_closed FROM users WHERE username='$username'");        
+        $row = mysqli_fetch_array($query);
+
+        if($row['user_closed'] == 'yes')
+            return true;
+        else
+            return false;    
     }
 }
 ?>
